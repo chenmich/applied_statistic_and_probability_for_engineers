7dce08c76b558c76a449708d5d0c7a75560d002d
chenmich <403189920@qq.com>
2021-08-04 15:52:03 +0800

    complete first

---
+++ docs/js/math_equation_numbering.js
@@ -0,0 +1,12 @@
+MathJax.Hub.Config({
+  "tex2jax": { 
+    inlineMath: [ [ '$', '$' ] ],
+    ignoreClass: "tex2jax_ignore"
+  }
+});
+MathJax.Hub.Config({
+config: ["MMLorHTML.js"],
+jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+extensions: ["MathMenu.js", "MathZoom.js"],
+TeX: { equationNumbers: { autoNumber: "AMS" } }
+});
